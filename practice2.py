80bda3a9eab88a6a00f5e3567d6cb97601bc0b1b
niuhaoyu16 <32653950+niuhaoyu16@users.noreply.github.com>
2020-04-13 18:58:45 -0700

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 13 17:39:19 2020
+
+@author: niuha
+"""
+
+import torch
+
+dtype = torch.float
+#device = torch.device("cpu")
+
+device = torch.device("cuda:0")
+
+N, D_in, H, D_out = 64,1000,100,10
+
+x = torch.randn(N, D_in, device = device, dtype=dtype)
+y = torch.randn(N, D_out, device = device, dtype=dtype)
+
+w1 = torch.randn(D_in, H, device=device, dtype=dtype)
+w2 = torch.randn(H, D_out, device=device, dtype=dtype)
+
+lr = 1e-6
+for t in range(500):
+    h = x.mm(w1)
+    h_relu = h.clamp(min=0)
+    y_pred = h_relu.mm(w2)
+    
+    loss = (y_pred -y).pow(2).sum().item()
+    if t % 100 ==99:
+        print(t,loss)
+        
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_w2 = h_relu.t().mm(grad_y_pred)
+    grad_h_relu = grad_y_pred.mm(w2.t())
+    grad_h = grad_h_relu.clone()
+    grad_h[h < 0] = 0
+    grad_w1 = x.t().mm(grad_h)
+    
+    w1 -= lr * grad_w1
+    w2 -= lr * grad_w2
